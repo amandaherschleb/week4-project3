07e666842cfe86f330fc2ea8021a26fa9951dd8c
Amanda Herschleb <amanda.herschleb@gmail.com>
2016-08-15 18:46:31 -0600

    bonus might need work

--- main.js
+++ main.js
@@ -2,18 +2,24 @@ $(document).ready(function(){
 // 1. The text under "Click for short bio" should not be visible unless the user clicks the flip id.
 //    Hint: Google the jQuery slideToggle function
 
+$('#flip').click(function(){
+  $('#panel').slideToggle('slow');
+});
 
 
-
-// 2. When hovering over the h4 tags (Objective, Experience, etc..), 
+// 2. When hovering over the h4 tags (Objective, Experience, etc..),
 //    the inside of the dotted box should turn light blue and remain light blue.
 
-
+$('h4').hover(function(){
+  $(this).css('background-color','lightblue');
+});
 
 
 // 3. When any of the project images is clicked, it should expand to 300px wide.
 
-
+$('.project-image').click(function(){
+  $(this).addClass('clicked-image');
+});
 
 
 
@@ -21,23 +27,52 @@ $(document).ready(function(){
 //    Additionally, the h4 tag hover that you created in #2 should turn white.
 //    Also, the background should change to the dark_embroidery.png image located the images directory.
 
+var clickCount=0;
 
+$('#low-vision').click(function(){
+  clickCount+=1;
+  $('body').toggleClass('changeTextColors');
+  if(clickCount%2 === 0) {
+    $('body').css('background','url("images/brickwall.png")');
+    $('h4').hover(function(){
+      $(this).css('background-color','lightblue');
+    });
+  } else {
+    $('body').css('background','url("images/dark_embroidery.png")');
+    $('h4').hover(function(){
+      $(this).css('background-color','white');
+    });
+  }
+});
 
 
 // 5. When hovering over the Twitter, Instagram, and LinkedIn buttons, the background behind the buttons should turn #6bd2db.
 //    This has not been loaded into a CSS class and must be done within the function.
 //    Make sure you get those corners rounded so the color doesn't pop out in the corners!
-  
-  
-  
+
+$('#social-links img').hover(function(){
+  $(this).toggleClass('socialBackground');
+});
+
+
 
 //BONUS
-//       The contact area contains a span that can be clicked to reveal the contact info. 
-//       The HTML and CSS have been written for you to enable this functionality with jQuery.  
+//       The contact area contains a span that can be clicked to reveal the contact info.
+//       The HTML and CSS have been written for you to enable this functionality with jQuery.
 //       The user should click a button next to the person's name to reveal the phone number and email address.
 
- 
- 
- 
- 
-}
+$('.reference-contact1,.reference-contact2').hide();
+
+
+$('.btn1').click(function(){
+  $(this).hide();
+  $('.reference-contact1').show();
+});
+
+$('.btn2').click(function(){
+  $(this).hide();
+  $('.reference-contact2').show();
+});
+
+
+});
